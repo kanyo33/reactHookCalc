5fb4540fc06d51039b63e876683a4e064f5f7a19
Noah <n-k@live.ca>
2019-10-19 12:06:54 +0200

    Updates

---
+++ precache-manifest.c57bc390832747219683a5c1ec9da454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1061818864083ed6a018",
+    "url": "/reactHookCalc/static/js/runtime~main.4311b0ea.js"
+  },
+  {
+    "revision": "44e165b843c9695f5d70",
+    "url": "/reactHookCalc/static/js/main.10f5dc7d.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/reactHookCalc/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "44e165b843c9695f5d70",
+    "url": "/reactHookCalc/static/css/main.3ec21dcb.chunk.css"
+  },
+  {
+    "revision": "2a0a7c754bd9dc8deb07d97520f8e2af",
+    "url": "/reactHookCalc/index.html"
+  }
+];
